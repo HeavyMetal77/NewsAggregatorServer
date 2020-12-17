391754229e319c0e058abe7e4b67c6d224fc4ab0
HeavyMetal77 <ttm2006@ukr.net>
2020-12-17 13:01:34 +0200

    added filter max size db

--- src/main/java/ua/tarastom/news/LoadDatabase.java
+++ src/main/java/ua/tarastom/news/LoadDatabase.java
@@ -9,8 +9,10 @@ import org.springframework.context.annotation.Configuration;
 import ua.tarastom.news.data.SupplierData;
 import ua.tarastom.news.model.Article;
 
+import java.time.LocalDateTime;
 import java.util.Collection;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Configuration
 public class LoadDatabase {
@@ -24,6 +26,11 @@ public class LoadDatabase {
             while (true) {
                 List<Article> articles = SupplierData.loadRSS();
                 List<Article> all = articleRepository.findAll();
+                if (all.size() > 9800) {
+                    all = all.stream().sorted().limit(8000).collect(Collectors.toList());
+                    LocalDateTime pubDate = all.get(all.size() - 1).getPubDate();
+                    articles = articles.stream().filter((element)-> element.getPubDate().isAfter(pubDate)).collect(Collectors.toList());
+                }
                 Collection<Article> subtract = CollectionUtils.subtract(articles, all);
                 articleRepository.saveAll(subtract);
                 Thread.sleep(20000);
